efddcfd6b10500872e4d3eb9d542a8645824f24a
Rhys Gevaux <rhys-github@sandimas.co>
2021-08-11 15:45:08 +1000

    format

--- src/0-react-hooks/0-tic-tac-toe/types.ts
+++ src/0-react-hooks/0-tic-tac-toe/types.ts
@@ -2,8 +2,17 @@ import type { PLAYERS } from "./constants";
 
 export type Player = typeof PLAYERS[number];
 export type Move = { readonly index: number; readonly player: Player };
-export type AddMoveAction = { readonly type: "ADD_MOVE"; readonly payload: number };
+export type AddMoveAction = {
+  readonly type: "ADD_MOVE";
+  readonly payload: number;
+};
 export type ResetMovesAction = { readonly type: "RESET_MOVES" };
-export type SetActiveMoveAction = { readonly type: "SET_ACTIVE_MOVE"; readonly payload: number };
+export type SetActiveMoveAction = {
+  readonly type: "SET_ACTIVE_MOVE";
+  readonly payload: number;
+};
 export type Action = AddMoveAction | ResetMovesAction | SetActiveMoveAction;
-export type State = { readonly activeIndex: number; readonly moves: readonly Move[] };
+export type State = {
+  readonly activeIndex: number;
+  readonly moves: readonly Move[];
+};
